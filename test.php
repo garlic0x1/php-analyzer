c1f8ee81b479ca528016e7d0015e25f5512d9ab1
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2022-06-01 18:45:46 -0700

    Add files via upload

--- test.php
+++ test.php
@@ -2,17 +2,23 @@
 Class Data {
 	function dangerous($param) {
 		$temp = unknown_filter_func($param);
-		query($user_input);
-		query($_GET[]);
+		// this does not alert because the variable is not in scope
+		print($user_input);
 		return $temp;
 	}
 }
 
-if (($_GET)) {
-	echo $_GET;
-}
-
+$user_input = $_GET['input'];
+$improperly_filtered = "$user_input";
 $d = new Data;
 $t = $d->dangerous($_GET);
+
+// this does not alert because the input was sanitized
+query($improperly_filtered);
+
+// this does alert because magic quotes dont stop xss
+echo $improperly_filtered;
+
+// alerts because taint follows through method call into $t
 query($t);
 ?>
